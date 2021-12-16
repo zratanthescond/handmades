72061b1e1d91968d6bc5538648b6090b0fe985cd
Bilel oueslati <mrbileltn@gmail.com>
2021-12-16 11:45:28 +0100

    Update CRUD

--- src/Controller/Admin/Crud/BlogCommentCrudController.php
+++ src/Controller/Admin/Crud/BlogCommentCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin\Crud;
 
 use App\Entity\BlogComment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -24,6 +26,11 @@ class BlogCommentCrudController extends AbstractCrudController
         ->setDefaultSort(["createdAt" => "DESC"]);
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->disable(Action::NEW);
+    }
+     
     public function configureFields(string $pageName): iterable
     {
         return [
