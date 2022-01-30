b12322848e7b4200e4a5b6f603eebbc57cc310a1
Bilel Oueslati <b.oueslati@ewm.swiss>
2022-01-30 11:10:18 +0100

    Handle Aramex error

--- src/Service/Aramex/Aramex.php
+++ src/Service/Aramex/Aramex.php
@@ -269,6 +269,8 @@ class Aramex
 
             if ($data["HasErrors"] === true) {
 
+                dd($data);
+
                $errorMessage = AramexErrorHandler::getErrorMessage($data);
 
                 throw new AramexException($errorMessage);
